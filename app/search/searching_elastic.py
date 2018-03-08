8953ebb61bdb1bcc00e16e83439127e460b9aee2
Justin Liu <jxl3908@rit.edu>
2018-03-08 10:30:29 -0500

    host is there so why is it not working in heroku?

--- app/search/searching_elastic.py
+++ app/search/searching_elastic.py
@@ -7,8 +7,7 @@ def searching_elastic(search_text):
     """
     Creates an instance of elastic search and searches to see if it has results for specified search text.
     """
-    print "elastichost", os.environ.get("ELASTIC_HOSTS")
-    es = Elastic_Search(hosts=os.environ.get("ELASTIC_HOSTS"))
+    es = Elastic_Search(os.environ.get("ELASTIC_HOSTS"))
 
     result = es.search_elastic(search_text)
     display_array = []
