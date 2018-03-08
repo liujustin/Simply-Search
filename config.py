fdb9e742e8a40b7964cd4451f1374e78f355997e
Justin Liu <jxl3908@rit.edu>
2018-03-07 20:41:45 -0500

    heroku bonsai elasticsearch url

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     """
     POSTS_PER_PAGE = 5
     MONGO_URI = "mongodb://justinliu:246813579@ds259768.mlab.com:59768/enerknol"
-    ELASTIC_HOST = [{ "host" : "localhost", "port" : 9200 }]
+    ELASTIC_HOST = ['https://jsy0vkfpyo:nu4ueefwd7@holly-2620832.us-east-1.bonsaisearch.net']
 
 class DevelopmentConfig(Config):
     """
