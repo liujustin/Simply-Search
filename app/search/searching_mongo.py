71277b43b2130eb3e979d5a78ab74ab5b0db9718
Justin Liu <jxl3908@rit.edu>
2018-03-08 10:39:58 -0500

    frogot to fix mongo host as well

--- app/search/searching_mongo.py
+++ app/search/searching_mongo.py
@@ -7,6 +7,6 @@ def searching_mongo(mongo_id):
     """
     Search Mongo for a specific _id returned from elastic and returns the result.
     """
-    mongodb = MongoDB(host=os.getenv("MONGO_URI"))
+    mongodb = MongoDB(host="mongodb://justinliu:246813579@ds259768.mlab.com:59768/enerknol")
     mongo_result = mongodb.search_mongo(mongo_id)
     return mongo_result
